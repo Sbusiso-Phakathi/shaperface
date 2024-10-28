5f2afde5f03331786f9d38780edf02d9bf0815e1
Sbusiso Phakathi <phaks323@gmail.com>
2024-10-28 15:25:25 +0200

    Create FaceRecognition.js

---
+++ src/FaceRecognition.js
@@ -0,0 +1,13 @@
+// src/components/FaceRecognition.js
+
+import React, { useRef, useState, useEffect } from 'react';
+import Webcam from 'react-webcam';
+import axios from 'axios';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+
+function FaceRecognition() {
+  // ... rest of the FaceRecognition component code (as in the previous answer)
+}
+
+export default FaceRecognition;
